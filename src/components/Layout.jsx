9e4f33747af4c1a28d59325b2d66cdc2d6ad498f
Jimmy Østby <142933101+jimmyostby@users.noreply.github.com>
2025-03-03 12:12:08 +0100

    Prøvde å skrive ut forrige html.
    Pørvde å skrive ut tidligere html til nettsiden. Dette fungerte IKKE.

---
+++ src/components/Layout.jsx
@@ -0,0 +1,18 @@
+export default function Layout(){
+    return(
+        <body>
+   <header>
+        <nav>
+            <ul id="faneid">
+                <p>hei</p>
+            </ul>
+        </nav>
+    </header>
+   <main>
+        <section id="whitebox"> 
+        </section>
+    </main>
+    
+</body>
+    )
+}
